d733f16e8ce5a9c4849ffe447f7a28a4212de248
Nathanael <ngreese7@ou.edu>
2020-11-04 17:18:18 -0600

    Created a list of unique Jews

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Driver {
 
@@ -23,7 +24,32 @@ public class Driver {
 		// Assignment time
 		
 		//Set to find unique elements in a list
+		ArrayList<Jews> jewList = new ArrayList<Jews>();
+		jewList.add(new Jews("Netanel"));
+		jewList.add(new Jews("Moshe"));
+		jewList.add(new Jews("Moshe"));
+		jewList.add(new Jews("Moshe"));
+		jewList.add(new Jews("Rabbi Ovadia"));
+		jewList.add(new Jews("Jerry Seinfeld"));
+		jewList.add(new Jews("Netanel"));
+		jewList.add(new Jews("Sarah"));
+		jewList.add(new Jews("Sarah"));
+		jewList.add(new Jews("Sarah"));
+		jewList.add(new Jews("Spencer"));
+		jewList.add(new Jews("Kasi"));
+		jewList.add(new Jews("Zack"));
+		System.out.println("Too many Moshe's!!");
+		for(int i = 0; i < jewList.size(); ++i) {
+			System.out.println(jewList.get(i).getName());
+		}
+		System.out.println("\n");
+		HashSet<Jews> uniqueJews = new HashSet<Jews>(jewList);
 		
+		System.out.println("Now we have some unique Jews");
+		for (Jews jews : uniqueJews) {
+			System.out.println(jews.getName());
+		}
+		System.out.println("\n");
 		
 		// HashMap v. TreeMap
 		ArrayList<String> parshat = new ArrayList<String>();
@@ -37,8 +63,12 @@ public class Driver {
 		ParshatTorah hashTorah = new ParshatTorah(parshat);
 		System.out.println("Hash map of Torah Portions unsorted:");
 		hashTorah.printHash();
+		System.out.println("\n");
 		System.out.println("Tree Map of Torah Portions in alphabetical order");
 		hashTorah.printTree();
+		System.out.println("\n");
+		//Use tree map if organizing by value in ascending order or in ascending alphabetical order
+		//HashMap is really if you know the keys and where they are
 	}
 
 }
--- src/Jews.java
+++ src/Jews.java
@@ -1,4 +1,11 @@
 
 public class Jews {
-
+	private String jewName;
+	
+	public Jews(String name) {
+		jewName = name;
+	}
+	public String getName() {
+		return jewName;
+	}
 }
