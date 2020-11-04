8dbbceae81d0a36a71aaeca814b3f8f0de1379af
Nathanael <ngreese7@ou.edu>
2020-11-04 16:31:22 -0600

    Added Notes

--- src/Driver.java
+++ src/Driver.java
@@ -1,9 +1,21 @@
+import java.util.ArrayList;
 
 public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArrayList<Integer> notes = new ArrayList<Integer>();
+		Integer[] notes2 = new Integer[10];
+		// both can have null and duplicate elements
+		//both unordered
+		//use index to access
+		// ARraylist cannot use primitive types
+		//deck list queue and set...no beans
+		//AbstractList, AbstractQueue, AbstractSet, ArrayDecque, ArrayList, EnumSet, HashSet, 
+		//LinkedList, Stack, TreeSet, Vector???
+		
+		ArrayList<Number> nodes = new ArrayList<Number>();
+		ArrayList<Number> nodesRef = new ArrayList<Number>();
 	}
 
 }
